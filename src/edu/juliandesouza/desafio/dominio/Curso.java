e1f32a432ddf3d2dcdd7a1619409347c3db19a86
juliandesouza <map.julian@gmail.com>
2023-02-17 16:05:57 -0300

    last update

--- src/edu/juliandesouza/desafio/dominio/Curso.java
+++ src/edu/juliandesouza/desafio/dominio/Curso.java
@@ -4,11 +4,7 @@ public class Curso {
     private String titulo;
     private String descricao;
     private int cargaHoraria;
-
-    public Curso(){
-        
-    }
-    
+ 
     public String getTitulo() {
         return titulo;
     }
--- src/edu/juliandesouza/desafio/dominio/Mentoria.java
+++ src/edu/juliandesouza/desafio/dominio/Mentoria.java
@@ -1,15 +1,10 @@
 package edu.juliandesouza.desafio.dominio;
-
 import java.time.LocalDate;
 
 public class Mentoria {
     private String titulo;
     private String descricao;
     private LocalDate data;
-
-    public Mentoria(){
-        
-    }
 
     public String getTitulo() {
         return titulo;
